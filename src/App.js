d765d22dab555449228e8a3e5d731ecf2550096e
Antony-Baby <antonybabye@gmail.com>
2021-10-19 22:01:00 +0530

    initial commit 1.2

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ function App() {
   const topMovies = `discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`
   const newMovies = `discover/movie?api_key=${API_KEY}&language=en-US&sort_by=release_date.desc`
   const malMovies = `discover/movie?api_key=${API_KEY}&sort_by=release_date.desc&with_original_language=ml`
-  
+  const tamilMovies = `discover/movie?api_key=${API_KEY}&sort_by=release_date.desc&with_original_language=ta`
+  const hindiMovies = `discover/movie?api_key=${API_KEY}&sort_by=release_date.desc&with_original_language=hi`
 
   
+
     return ( 
     <div>
       <Header/>
       <RowMovies url={topMovies} title="Top Rated Movies"/>
       <RowMovies url={newMovies} title="New Movies"/>
       <RowMovies url={malMovies} title="Malayalam Movies"/>
+      <RowMovies url={tamilMovies} title="Tamil Movies"/>
+      <RowMovies url={hindiMovies} title="Hindi Movies"/>
+
 
     
    </div>        
